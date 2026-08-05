5381b16d723bda193992b39a76956cd4b4c80793
Wahyudi <wap.youdi@gmail.com>
2026-08-05 20:44:57 +0700

    Update firebase-config.js

--- app/bandarfinance/v1/firebase/firebase-config.js
+++ app/bandarfinance/v1/firebase/firebase-config.js
@@ -5,12 +5,12 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAu8Dj0gd84MUQyN86189c2S_4xYUXJ5MI",
+  authDomain: "bandar-finance.firebaseapp.com",
+  projectId: "bandar-finance",
+  storageBucket: "bandar-finance.firebasestorage.app",
+  messagingSenderId: "914920751325",
+  appId: "1:914920751325:web:d53e197a2a87a4fd95db34",
 };
 
 const app = initializeApp(firebaseConfig);
